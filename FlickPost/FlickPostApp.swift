cf971bf4c312e8321b3864a480be4e38ae5fb37b
Tyler Watkins <TylerWatkins0513@gmail.com>
2024-11-04 21:37:00 -0500

    Initial Commit

---
+++ FlickPost/FlickPostApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickPostApp.swift
+//  FlickPost
+//
+//  Created by Tyler Watkins on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickPostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickPostTests/FlickPostTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickPostTests.swift
+//  FlickPostTests
+//
+//  Created by Tyler Watkins on 11/4/24.
+//
+
+import Testing
+@testable import FlickPost
+
+struct FlickPostTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
